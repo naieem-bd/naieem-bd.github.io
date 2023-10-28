c1dcd4d45fc9497ae973f6221515b895bd8b1d06
naieem-bd <naieemur@gmail.com>
2023-10-28 20:40:42 +0600

    init commit

---
+++ assets/js/active.js
@@ -0,0 +1,97 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+        
+        /*---
+        * Sticky
+        *---------------*/
+        $('#mainmenu').sticky({topSpacing:0});
+		
+		
+        /*---
+        * One Page Nav Menu
+        *---------------*/		
+		$('.navbar-nav').onePageNav({
+			currentClass : 'active',
+			scrollSpeed : 600,
+			filter: ':not(.url)'
+		});
+		
+		
+        /*---
+        * jQuery Smooth Scroll
+        *-------------------------*/
+		$('.scroll-down, .navbar-nav li a').on('click', function(event) {
+ 		   $('.navbar-nav li a').parent().removeClass('active');
+            var $anchor = $($(this).attr('href')).offset().top - 35;
+ 		   $(this).parent().addClass('active');
+            $('body, html').animate({scrollTop : $anchor}, 600);
+ 					 event.preventDefault();
+            return false;
+        });		
+        
+        
+        /*---
+        * Bootstrap Mobile Menu Fix
+        *-------------------------*/		
+        $('#navMenu li a').on('click', function() {
+            $('#navMenu').removeClass('in');
+        });		
+		
+        
+        /*---
+        * Images Loaded & Isotope
+        *-------------------------*/		
+        $('.grid').imagesLoaded(function() {
+            // filter items on button click
+            $('.portfolio-menu').on('click', 'button', function() {
+                var filterValue = $(this).attr('data-filter');
+                $grid.isotope({
+                    filter: filterValue
+                });
+            });
+            // init Isotope
+            var $grid = $('.grid').isotope({
+                itemSelector: '.grid-item',
+                percentPosition: true,
+                masonry: {
+                    // use outer width of grid-sizer for columnWidth
+                    columnWidth: '.grid-item',
+                }
+            });
+        });
+		
+		
+        /*---
+        * Portfolio Menu
+        *-------------------------*/		
+        $('.portfolio-menu button').on('click', function(event) {
+            $(this).siblings('.active').removeClass('active');
+            $(this).addClass('active');
+            event.preventDefault();
+        });
+		
+		
+        /*---
+        * Magnific Popup
+        *-------------------------*/	        
+        $(".portfolio-lightbox").magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+        
+
+		
+    });
+	
+    
+	$(window).load(function(){
+		
+
+		
+	});
+
+})(jQuery);
